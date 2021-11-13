d526bb97fe43c068275ffc31ba13729afced47d6
Léopold Goudier <leopold.goudier@gmail.com>
2021-11-13 16:40:15 +0100

    module newperso

--- new_perso.py
+++ new_perso.py
@@ -1,11 +1,36 @@
 from tkinter import * 
 from tkinter.messagebox import *
 from PIL import ImageTk, Image
-import tkinter.font as tkFont
+from fabriquecreature import FabriqueCreature
 
-class NewPerso():
+class NewPerso:
 
-    def __init__(self):
+    def __init__(self, name="Claude"):
 
         self.screen = Tk()
+        self.screen.title("Création d'un nouveau personnage")
+        w, h = self.screen.winfo_screenwidth(), self.screen.winfo_screenheight()
+        self.screen.geometry("%dx%d" % (w, h))
+
+        self.player = FabriqueCreature.get_creature("perso", name)
+
+        IMG_Image = PhotoImage(file="d.png")
+        CAN_Zone = Canvas(self.screen, height=h, width=w)
+        CAN_Zone_Image = CAN_Zone.create_image(0, 0, image=IMG_Image, anchor="nw")
         
+        img = ImageTk.PhotoImage(Image.open("spriteTest.png"))  
+        CAN_Sprite_Image = CAN_Zone.create_image(768, 200, image=img, anchor="n")
+        
+        congratulation = CAN_Zone.create_text(750, 60, text="Bravo ! Tu viens de creer ton personnage.", font=("Roman", 40), fill="#6d1212")
+        charaName = CAN_Zone.create_text(750, 130, text=f"Bienvenue {name}", font=("Roman", 40), fill="#6d1212")
+        
+        stats = CAN_Zone.create_rectangle(145, 300, 445, 446, width=2)
+        statsStrength = CAN_Zone.create_text(295, 340, text=f"Force : {self.player.strength} Mana", font=("Roman", 30), fill="#6d1212")
+        statsHp = CAN_Zone.create_text(295, 400, text=f"Vie : {self.player.hp} Hp", font=("Roman", 30), fill="#6d1212")
+
+        CAN_Zone.pack()
+        
+        self.screen.mainloop()
+
+if __name__ == "__main__":
+    NewPerso()
