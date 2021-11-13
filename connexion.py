dbb8eb9540477a6cd9ee57469eae058a850d6e58
Léopold Goudier <leopold.goudier@gmail.com>
2021-11-13 14:07:53 +0100

    page connexion finit, et todo actualisé

--- connexion.py
+++ connexion.py
@@ -32,78 +32,85 @@ class Connection:
             writer=csv.writer(r)
             writer.writerow([self.username, self.password])
 
-#class ConnectionPage:
+class ConnectionPage:
+
+    def verify(self, id_button):
+            if entreeUser.get() != "" and entreePassword.get() != "":
+                connection = Connection(entreeUser.get(), entreePassword.get())
+                if id_button == 1:
+                    if connection.login():
+                        print("Connecté")
+                    else:
+                        showerror("Erreur", "Votre nom d'utilisateur ou votre mot de passe est incorrect.")
+                        #Envoyé par page du menu
+                elif id_button == 2:
+                    if not connection.already_registered_or_not():
+                        connection.register()
+                        print("Enregistré")
+                        #Envoyé sur page du menu
+                    else:
+                        showinfo("Erreur", "Ce nom d'utilisateur est déjà prit.")
+            else:
+                showerror("Erreur", "Veuillez remplir votre nom d'utilisateur et votre mot de passe.")
+
+    def __init__(self):
+        
+        self.screen = Tk() 
+        self.screen.title("Connexion")
+        w, h = self.screen.winfo_screenwidth(), self.screen.winfo_screenheight()
+        self.screen.geometry("%dx%d" % (w, h))   
+        self.screen.config(background="#282c34")
+
+        """
+        background_image= PhotoImage("pixelBG.png")
+        background_label = Label(self.screen, image=background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        """
+
+        #Frame Titre du Jeu
+        Frame1 = Frame(self.screen, height=80, width=200, borderwidth=10, bg="black")
+        Frame1.grid(column=1, row=0, pady=50)
+        #Frame Sprite gauche
+        Frame2 = Frame(self.screen, height=500, width=231, borderwidth=1, bg="#282c34")
+        Frame2.grid(column=0, row=1, padx=120)
+        #Frame Menu de connexion
+        Frame3 = Frame(self.screen, height=80, width=200, borderwidth=10, bg="#fafaee")
+        Frame3.grid(column=1, row=1)
+        #Frame Sprite droit
+        Frame4 = Frame(self.screen, height=500, width=231, borderwidth=1, bg="#282c34")
+        Frame4.grid(column=2, row=1, padx=120)
+
+        labelTitle = Label(Frame1, text="Adventuria", fg="white", bg="#7f5fdd", font=("Roman", 80, "bold"))
+        labelTitle.grid(column=0, row=0)
+
+        img = ImageTk.PhotoImage(Image.open("spriteTest.png"))  
+        
+        label1 = Label(Frame2, image=img, bg="#282c34", width=231, height=500)
+        label1.pack(expand=YES)
+        
+        label2 = Label(Frame4, image=img, bg="#282c34", width=231, height=500)
+        label2.pack(expand=YES)
+
+        labelUser = Label(Frame3, text="Nom d'utilisateur :", fg="#8601af", bg="#fafaee", font=("Roman", 30))
+        labelUser.grid(column=1, row=0)
+        
+        valueUser = StringVar()
+        entreeUser = Entry(Frame3, textvariable=valueUser, width=30)
+        entreeUser.grid(column=1, row=1, pady=20)
+
+        labelPassword = Label(Frame3, text="Mot de passe :", fg="#8601af", bg="#fafaee", font=("Roman", 30))
+        labelPassword.grid(column=1, row=2)
+        
+        valuePassword = StringVar()
+        entreePassword = Entry(Frame3, textvariable=valuePassword, width=30)
+        entreePassword.grid(column=1, row=3, pady=20)
+
+        Button(Frame3, text ='Connexion', command=lambda: self.verify(1), fg="#8601af", font=("Ebrima", 12)).grid(column=0, row=4, pady=10, padx=10)
+        Button(Frame3, text ='Inscription', command=lambda: self.verify(2), fg="#8601af", font=("Ebrima", 12)).grid(column=2, row=4 ,pady=10, padx=10)
+
+        self.screen.mainloop()
+
 if __name__ == "__main__":
+    ConnectionPage()
 
-    def verify(id_button):
-        if entreeUser.get() != "" and entreePassword.get() != "":
-            connection = Connection(entreeUser.get(), entreePassword.get())
-            if id_button == 1:
-                if connection.login():
-                    print("Connecté")
-                else:
-                    showerror("Erreur", "Votre nom d'utilisateur ou votre mot de passe est incorrect.")
-                    #Envoyé par page du menu
-            elif id_button == 2:
-                if not connection.already_registered_or_not():
-                    connection.register()
-                    print("Enregistré")
-                    #Envoyé sur page du menu
-                else:
-                    showinfo("Erreur", "Ce nom d'utilisateur est déjà prit.")
-        else:
-            showerror("Erreur", "Veuillez remplir votre nom d'utilisateur et votre mot de passe.")
-
-    fenetre = Tk()
-    w, h = fenetre.winfo_screenwidth(), fenetre.winfo_screenheight()
-    fenetre.geometry("%dx%d" % (w, h))   
-    fenetre.title("Connexion")
-    fenetre.config(background="#282c34")
-
-    """
-    background_image= PhotoImage("pixelBG.png")
-    background_label = Label(fenetre, image=background_image)
-    background_label.place(x=0, y=0, relwidth=1, relheight=1)
-    """
-    #Frame Titre du Jeu
-    Frame1 = Frame(fenetre, height=80, width=200, borderwidth=10, bg="black")
-    Frame1.grid(column=1, row=0, pady=50)
-    #Frame Sprite gauche
-    Frame2 = Frame(fenetre, height=500, width=231, borderwidth=1, bg="#282c34")
-    Frame2.grid(column=0, row=1, padx=120)
-    #Frame Menu de connexion
-    Frame3 = Frame(fenetre, height=80, width=200, borderwidth=10, bg="#fafaee")
-    Frame3.grid(column=1, row=1)
-    #Frame Sprite droit
-    Frame4 = Frame(fenetre, height=500, width=231, borderwidth=1, bg="#282c34")
-    Frame4.grid(column=2, row=1, padx=120)
-
-    labelTitle = Label(Frame1, text="Adventuria", fg="white", bg="#7f5fdd", font=("Roman", 80, "bold"))
-    labelTitle.grid(column=0, row=0)
-
-    img = ImageTk.PhotoImage(Image.open("spriteTest.png"))  
-    
-    label1 = Label(Frame2, image=img, bg="#282c34", width=231, height=500)
-    label1.pack(expand=YES)
-    
-    label2 = Label(Frame4, image=img, bg="#282c34", width=231, height=500)
-    label2.pack(expand=YES)
-
-    labelUser = Label(Frame3, text="Nom d'utilisateur :", fg="#8601af", bg="#fafaee", font=("Roman", 30))
-    labelUser.grid(column=1, row=0)
-    
-    valueUser = StringVar()
-    entreeUser = Entry(Frame3, textvariable=valueUser, width=30)
-    entreeUser.grid(column=1, row=1, pady=20)
-
-    labelPassword = Label(Frame3, text="Mot de passe :", fg="#8601af", bg="#fafaee", font=("Roman", 30))
-    labelPassword.grid(column=1, row=2)
-    
-    valuePassword = StringVar()
-    entreePassword = Entry(Frame3, textvariable=valuePassword, width=30)
-    entreePassword.grid(column=1, row=3, pady=20)
-
-    Button(Frame3, text ='Connexion', command=lambda: verify(1), fg="#8601af", font=("Ebrima", 12)).grid(column=0, row=4, pady=10, padx=10)
-    Button(Frame3, text ='Inscription', command=lambda: verify(2), fg="#8601af", font=("Ebrima", 12)).grid(column=2, row=4 ,pady=10, padx=10)
-
-    fenetre.mainloop()
+        
